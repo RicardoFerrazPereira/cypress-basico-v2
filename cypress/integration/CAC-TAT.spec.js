4593fe0e845f6d2128e6a6e3c52d9cbc108da83e
ricardo_ferraz <ricardo.ferraz@medportal.com.br>
2023-05-18 11:29:50 -0300

    aula12 exercicio extra3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -25,6 +25,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
     })
 
+    // AULA 9 -> EXERCICIO / AULA 10 -> EXERCICIO EXTRA 1
+
     it('preenche os campos obrigatórios e envia o formulário', function() {
         const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
 
@@ -38,15 +40,29 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.success').should('be.visible') // pegar o elemento com a classe success e deixa-lo visível
     })
 
-    // AULA 10 -> EXERCICIO EXTRA 2 (lession 02.md)
+    // AULA 11 -> EXERCICIO EXTRA 2 (lession 02.md)
 
-    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+    // Crie um teste chamado exibe mensagem de erro ao submeter o formulário com um email com formatação inválida
+    // Tal teste deve verificar que uma mensagem é exibida em um elemento com a classe error
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
         cy.get('#firstName').type('Ricardo')
         cy.get('#lastName').type('Ferraz')
         cy.get('#email').type('ricardo@exemplo.com') // adicionar um email ívalido
         cy.get('#open-text-area').type('Testando')
         cy.get('button[type="submit"]').click() 
         cy.get('.error').should('be.visible')
+    })
+
+    // AULA12 -> EXERCICIO EXTRA 3
+
+    // Visto que o campo de telefone só aceita números, crie um teste para validar que, 
+    // se um valor não-numérico for digitado, seu valor continuará vazio.
+
+    it.only('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
     })
 
   })
